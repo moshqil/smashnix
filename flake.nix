8e1e4d399e5f617626470236c55634b3fb7ed2a7
Maria Marchenko <maria.m.marchenko@mail.ru>
2022-10-11 23:15:55 +0200

    Initialize nix config

---
+++ flake.nix
@@ -0,0 +1,73 @@
+{
+  description = "MASH SMASH ULTIMATE NIX CONFIG 1337";
+
+  inputs = {
+    # Package sets
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-22.05-darwin";
+    nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+
+    # Environment/system management
+    darwin.url = "github:lnl7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/release-22.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Other sources
+    comma = { url = github:Shopify/comma; flake = false; };
+  };
+
+  outputs = { self, darwin, nixpkgs, home-manager, ... }@inputs:
+  let 
+
+    inherit (darwin.lib) darwinSystem;
+    inherit (inputs.nixpkgs.lib) attrValues makeOverridable optionalAttrs singleton;
+
+    # Configuration for `nixpkgs`
+    nixpkgsConfig = {
+      config = { allowUnfree = true; };
+    }; 
+  in
+  {
+    # My `nix-darwin` configs
+      
+    darwinConfigurations = rec {
+      Marias-Air = darwinSystem {
+        system = "aarch64-darwin";
+        modules = [ 
+          # Main `nix-darwin` config
+          ./configuration.nix
+          # `home-manager` module
+          home-manager.darwinModules.home-manager
+          {
+            nixpkgs = nixpkgsConfig;
+            # `home-manager` config
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.mashsmash = import ./home.nix;            
+          }
+        ];
+      };
+    };
+
+    # Overlays --------------------------------------------------------------- {{{
+
+    overlays = {
+      # Overlays to add various packages into package set
+        comma = final: prev: {
+          comma = import inputs.comma { inherit (prev) pkgs; };
+        };  
+
+      # Overlay useful on Macs with Apple Silicon
+        apple-silicon = final: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
+          # Add access to x86 packages system is running Apple Silicon
+          pkgs-x86 = import inputs.nixpkgs {
+            system = "x86_64-darwin";
+            inherit (nixpkgsConfig) config;
+          };
+        }; 
+      };
+
+      # TODO: sudo with touch id
+      # https://github.com/LnL7/nix-darwin/pull/228
+ };
+}
---
+++ home.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, lib, ... }:
+
+{
+  home.stateVersion = "22.05";
+
+  # https://github.com/malob/nixpkgs/blob/master/home/default.nix
+
+  # Direnv, load and unload environment variables depending on the current directory.
+  # https://direnv.net
+  # https://rycee.gitlab.io/home-manager/options.html#opt-programs.direnv.enable
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
+
+  # Htop
+  # https://rycee.gitlab.io/home-manager/options.html#opt-programs.htop.enable
+  programs.htop.enable = true;
+  programs.htop.settings.show_program_path = true;
+
+  programs.git = {
+    enable = true;
+    userEmail = "maria.m.marchenko@mail.ru";
+    userName = "Maria Marchenko";
+    delta.enable = true;
+    extraConfig = {
+      pull.rebase = true;
+    };
+  };
+
+  home.packages = with pkgs; [
+    # Some basics
+    coreutils
+    curl
+    wget
+    jq
+    ripgrep
+    tmate
+    just
+    git
+  ] ++ lib.optionals stdenv.isDarwin [
+    m-cli # useful macOS CLI commands
+  ];
+}
